7ffc450d8b817e903fd326b5e757e865d7ab9489
pythonaccount <119372601+pythonaccount@users.noreply.github.com>
2022-12-02 12:27:40 +0530

    Add files via upload

--- repositary_text.py
+++ repositary_text.py
@@ -13,4 +13,16 @@ def greet(func):
 
 
 greet(shout)
-greet(whisper
+greet(whisper)
+
+
+def create_adder(x):
+    def adder(y):
+        return x + y
+
+    return adder
+
+
+add_15 = create_adder(15)
+
+print(add_15(10))
